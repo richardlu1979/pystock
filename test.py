894317e69782c34e275f198e77519dcd3a7a51eb
luchao <295702488@qq.com>
2018-03-02 17:27:10 +0800

    修改test第三次

--- test.py
+++ test.py
@@ -1,7 +1,10 @@
+#coding:utf-8
 import tushare as ts
 
-a=4
-print a
 
-b=5
-print b
+#一段小脚本，把笔记里面记录的红电波推荐的票，计算下3，5，10日内的价格变动情况
+#example: 海南瑞泽  2018-02-26（推荐买入时间）
+
+hn=ts.get_k_data('002596',start='2018-02-26',end='2018-03-01')
+
+print hn
