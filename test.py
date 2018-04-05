d7c624c85d8011740e2a6c260d3b15be4905b2c6
luchao <295702488@qq.com>
2018-04-05 23:11:57 +0800

    modify test.py

--- test.py
+++ test.py
@@ -5,12 +5,19 @@ import tushare as ts
 #一段小脚本，把笔记里面记录的红电波推荐的票，计算下3，5，10日内的价格变动情况
 #example: 海南瑞泽  2018-02-26（推荐买入时间）
 
+
+
+
 hn=ts.get_k_data('002596',start='2018-02-26',end='2018-03-01')
 
-print hn
+print  (hn)
 
-a='test'
-print a
+print ('=============')
 
-b='test2'
-print b
+data = open('stocklist.txt')
+for each_line in data:
+    (buytime,stockname) = each_line.split(' ',1)
+    print (buytime,end='')
+    print (' is ',end='')
+    print (stockname,end='')
+
