48bbf575fb7cdcd89cb0d862e5f8fe504d960004
luchao <295702488@qq.com>
2018-04-09 11:55:31 +0800

    modify 04-09

--- test.py
+++ test.py
@@ -7,9 +7,9 @@ hn=ts.get_k_data('300584',start='2018-04-03')
 #print (hn)
 #判断n天后是涨 y=1，是跌 y=0
 y=0
-n=1
+n=4
 sbegin = hn.iloc[0].values[2]
-send = hn.iloc[n].values[2]
+send = hn.iloc[1].values[2]
 
 if send>sbegin:
     y=1
@@ -17,6 +17,21 @@ if send>sbegin:
 #print(sbegin)
 #print(send)
 #print(y)
+
+'''
+   
+'''
+def isrise(btime,sname,span):
+    try:
+        tmphn = ts.get_k_data(stockname.strip(),start=buytime.strip())
+        tmpbegin = tmphn.iloc[0].values[2]
+        tmpend = tmphn.iloc[n].values[2]
+        if tmpend > tmpbegin:
+            return 1
+        else:
+            return 0
+    except:
+        return -1
 
 
 
@@ -28,15 +43,15 @@ nrise =0
 for each_line in data:
     ntotal = ntotal +1
     (buytime,stockname) = each_line.split(' ',1)
-    tmphn = ts.get_k_data(stockname.strip(),start=buytime.strip())
-    print(tmphn)
-    tmpbegin = tmphn.iloc[0].values[2]
-    tmpend = tmphn.iloc[1].values[2]
-    if tmpend>tmpbegin:
-        nrise = nrise +1
-    print(nrise)
-  
+    k= isrise(buytime,stockname,n)
+    if k<0:
+        ntotal = ntotal -1
+    else:
+        nrise = nrise +k
+
 print ('==home====')
 print (ntotal)
+print(nrise)
+print(100*nrise/ntotal)
 
 
