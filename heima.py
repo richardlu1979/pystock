5c79b8cd7e1d6418f07ce6e9ddaacd74b5769e1f
chao <295702488@qq.com>
2019-04-21 00:05:12 +0800

    4-21 修改，diff的计算和同花顺的结果不一样。。。

--- heima.py
+++ heima.py
@@ -15,42 +15,131 @@ import os
 import time
 import sqlite3
 import talib
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
 
 '''
-    testts: 测试tushare
+    简单的策略——移动均线策略。这个策略是由下向上超过均线就买入，相反就卖出
+    simple: 测试tushare
+    返回值：null
+'''
+
+def simple():
+
+    df=ts.get_hist_data('600848',start='2017-01-01',end='2017-09-15')
+    df=df.sort_index()
+    df.index=pd.to_datetime(df.index,format='%Y-%m-%d')
+    #收市股价
+    close= df.close
+    #每天的股价变动百分率
+    ret=df.p_change/100
+    # 10日的移动均线为目标
+    df['SMA_10'] = talib.MA(np.array(close), timeperiod=10)
+    close10=df.SMA_10
+
+    #处理信号
+    SmaSignal=pd.Series(0,index=close.index)
+
+    for i in range(10,len(close)):
+        if all([close[i]>close10[i],close[i-1]<close10[i-1]]):
+            SmaSignal[i]=1
+        elif all([close[i]<close10[i],close[i-1]>close10[i-1]]):
+            SmaSignal[i]=-1
+
+    SmaTrade=SmaSignal.shift(1).dropna()
+
+    SmaBuy=SmaTrade[SmaTrade==1]
+
+    SmaSell=SmaTrade[SmaTrade==-1]
+
+    SmaRet=ret*SmaTrade.dropna()
+
+    #累积收益表现
+    #股票累积收益率
+    cumStock=np.cumprod(1+ret[SmaRet.index[0]:])-1
+    #策略累积收益率
+    cumTrade=np.cumprod(1+SmaRet)-1
+
+
+'''
+     doubleline: 双均线策略,即5日短线移动平均超过30日长线移动平均即买入，其余时候空仓
+    返回值：null
+'''
+
+def doubleline():
+    df = ts.get_hist_data('600848', start='2015-01-01', end='2015-12-31')
+    df = df.sort_index()
+    df.index = pd.to_datetime(df.index, format='%Y-%m-%d')
+    # 收市股价
+    close = df.close
+    # 每天的股价变动百分率
+    ret = df.p_change / 100
+    # 5\30日的移动均线为目标
+    df['SMA_5'] = talib.MA(np.array(close), timeperiod=5)
+    df['SMA_30'] = talib.MA(np.array(close), timeperiod=30)
+    close5 = df.SMA_5
+    close30 = df.SMA_30
+
+    # 处理信号
+    SmaSignal = pd.Series(0, index=close.index)
+
+    for i in range(10, len(close)):
+        if all([close5[i] > close30[i]]):
+            SmaSignal[i] = 1
+
+    SmaTrade = SmaSignal.shift(1).dropna()
+
+    SmaRet = ret * SmaTrade.dropna()
+
+    # 累积收益表现
+    # 股票累积收益率
+    cumStock = np.cumprod(1 + ret[SmaRet.index[0]:]) - 1
+    # 策略累积收益率
+    cumTrade = np.cumprod(1 + SmaRet) - 1
+
+
+'''
+    simplemacd: 即DIFF大于DEA的时候买入，其他时候卖出
     返回值：null
 '''
 
 
-def testts():
-    hn = ts.get_k_data('300701', start='2018-04-01')
-    sbegin = hn.iloc[0].values[2]
-    send = hn.iloc[1].values[2]
-    print sbegin
-    print send
-    print hn
-    # 测试获取股票实时报价
-    # df = ts.get_realtime_quotes('150174')  # Single stock symbol
-    # print(df[['code', 'name', 'price', 'bid', 'ask', 'volume', 'amount', 'time']])
+def simplemacd():
 
+    df = ts.get_hist_data('600848', start='2017-01-01', end='2017-12-31')
+    df = df.sort_index()
+    df.index = pd.to_datetime(df.index, format='%Y-%m-%d')
+    # 收市股价
+    close = df.close
+    # 每天的股价变动百分率
+    ret = df.p_change / 100
+    # 调用talib计算MACD指标
+    df['DIFF'], df['DEA'], df['MACD'] = talib.MACD(np.array(close),
+                                                   fastperiod=12, slowperiod=26, signalperiod=9)
 
-'''
-    watchstock: 监控实时的股票价格
-    返回值：null
-'''
+    diff = df.DIFF
+    dea = df.DEA
 
+    # 处理信号
+    macdSignal = pd.Series(0, index=close.index)
 
-def watchstock():
-    stocklist = ['150174']
-    while 1:
-        try:
-            for stock in stocklist:
-                df = ts.get_realtime_quotes(stock)  # Single stock symbol
-                print df[['name', 'price', 'time']]
+    for i in range(10, len(close)):
+        # if all([diff[i]>dea[i]>0,diff[i-1]<dea[i-1]]):
+        if all([diff[i] > dea[i]]):
+            macdSignal[i] = 1
+        # elif all([diff[i]<dea[i]<0,diff[i-1]>dea[i-1]]):
+        # macdSignal[i]=-1
 
-            time.sleep(1200)
-        except:
-            pass
+    macdTrade = macdSignal.shift(1).dropna()
+    macdRet = ret * macdTrade.dropna()
+
+    # 累积收益表现
+    # 股票累积收益率
+    cumStock = np.cumprod(1 + ret[macdRet.index[0]:]) - 1
+    # 策略累积收益率
+    cumTrade = np.cumprod(1 + macdRet) - 1
 
 
 
@@ -128,12 +217,63 @@ def stbupdate(stype):
         pass
 
 
+
+'''
+    heima: 
+           买入条件： 1）M10向上 2）收阳线 3）收盘价在MA10之上 4）DIF 在0之上
+           卖出条件： 1）M10向下 2）收阴线 3）收盘价在MA10之下  or 低于买入价10%
+    返回值：null
+'''
+def heima(stockname,sdate):
+
+    df = ts.get_hist_data(stockname, start=sdate)
+    df = df.sort_index()
+    df.index = pd.to_datetime(df.index, format='%Y-%m-%d')
+    # 收市股价
+    close = df.close
+    # 开盘股价
+    open = df.open
+    # MA10价格
+    ma10=df.ma10
+    # 每天的股价变动百分率
+    ret = df.p_change / 100
+    # 调用talib计算MACD指标
+    df['DIFF'], df['DEA'], df['MACD'] = talib.MACD(np.array(close),fastperiod=12, slowperiod=26, signalperiod=9)
+
+    diff = df.DIFF
+    dea = df.DEA
+
+    #晕死这个DIFF好像不对，所以还是直接输出其他几个判断条件后，列出来再人肉
+    # 处理信号
+    macdSignal = pd.Series(0, index=close.index)
+
+    for i in range(1, len(close)):
+        #if all([[diff[i]>0,ma10[i]>ma10[i-1]],close[i]>open[i],close[i]>ma10[i]]):
+        if all([[ma10[i] > ma10[i - 1]], close[i] > open[i], close[i] > ma10[i]]):
+            macdSignal[i] = 1
+        elif all([ma10[i]<= ma10[i-1],close[i] < open[i],close[i] < ma10[i]]):
+            macdSignal[i]=-1
+
+    macdTrade = macdSignal.shift(1).dropna()
+    macdRet = ret * macdTrade.dropna()
+
+    # 累积收益表现
+    # 股票累积收益率
+    cumStock = np.cumprod(1 + ret[macdRet.index[0]:]) - 1
+    # 策略累积收益率
+    cumTrade = np.cumprod(1 + macdRet) - 1
+    pd.set_option('display.max_rows', None)
+    print macdSignal
+
+
+
 '''
     主入口
 '''
 
 if __name__ == '__main__':
     print 'stock!'
+    heima('600036','2019-01-01')
 
 
 
